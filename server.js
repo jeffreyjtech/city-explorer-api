651884934bea2aa97e1dcd53007d872245c29ee4
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-22 23:49:40 -0800

    Added cors to server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 
 const { request, response } = require('express'); // eslint-disable-line
 const express = require('express');
+const cors = require('cors');
 const req = require('express/lib/request'); // eslint-disable-line
 
 const app = express();
+
+app.use(cors());
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
@@ -15,19 +18,25 @@ app.get('/weather', (request, response) => {
   // let lat = request.query.lat;
   // let lon = request.query.lon;
   let searchQuery = request.query.searchQuery;
+  console.log('I was pinged!');
 
   let foundWeather = weatherData.find(city => city.city_name === searchQuery);
+  let parsedWeatherData = {};
   if(foundWeather){
-    foundWeather.data.map(forecastData => new Forecast(forecastData));
+    parsedWeatherData = foundWeather.data.map(forecastData => new Forecast(forecastData));
   }
 
-  response.send(`Hi`);
+  response.send(parsedWeatherData);
+});
+
+app.get('*', (request, response) => {
+  response.send('You dun goofed');
 });
 
 class Forecast {
   constructor(data){
     this.date = data.datetime;
-    this.description = `Low of ${data.low_temp}, high of ${data.high_temp} with ${data.weather.description}`;
+    this.description = `Low of ${data.low_temp}, high of ${data.high_temp} with ${data.weather.description.toLowerCase()}.`;
   }
 }
 
