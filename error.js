e567173b5cd03f766cfa7005d8a38f3838914d77
Jeffrey Jenkins <97054263+jeffreyjtech@users.noreply.github.com>
2022-02-24 12:02:02 -0800

    Revert "Modularized API for Lab 09"

--- error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function errorResponse(error, request, response) {
-  error.status = error.status || 400;
-  error.message = `Error on request with route ${request.route.path || 'unknown'}: ${error.message}`;
-  response.status(error.status).send(`${error.message}`);
-}
-
-module.exports = errorResponse;
--- server.js
+++ server.js
@@ -4,21 +4,56 @@ const { request, response } = require('express'); // eslint-disable-line
 const express = require('express');
 const cors = require('cors');
 const req = require('express/lib/request'); // eslint-disable-line
+const axios = require('axios');
 
 const app = express();
-
-const getWeather = require('./weather.js');
-const getMovies = require('./movies.js');
 
 app.use(cors());
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
 
+// const weatherData = require('./data/weather.json');
 
-app.get('/weather', getWeather);
+function errorResponse(error, request, response) {
+  error.status = error.status || 400;
+  error.message = `Error on route ${request.route.path}: ${error.message}`;
+  response.status(error.status).send(`${error.message}`);
+}
 
-app.get('/movies', getMovies);
+app.get('/weather', async (request, response) => {
+  try {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=3&key=${process.env.WEATHER_API_KEY}`;
+
+    let foundWeather = await axios.get(url);
+
+    // This creates an array with shape [Forecast,Forecast,Forecast]
+    // Forecasts has shape {date: String, description: String}
+    let parsedWeatherData = foundWeather.data.data.map(forecastData => new Forecast(forecastData));
+    response.send(parsedWeatherData);
+  } catch (error) {
+    errorResponse(error, request, response);
+  }
+});
+
+app.get('/movies', async (request, response) => {
+  try {
+    let searchTerms = request.query.searchTerms;
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchTerms}&include_adult=false`;
+
+    let movieAPIData = await axios.get(url);
+
+    let movies = movieAPIData.data.results.map(result => new Movie(result));
+    response.send(movies);
+  } catch (error) {
+    errorResponse(error, request, response);
+  }
+});
+
 
 app.get('*', (request, response) => { // eslint-disable-line
   let newError = new Error;
@@ -26,6 +61,25 @@ app.get('*', (request, response) => { // eslint-disable-line
   newError.message = 'Resource not found';
   throw newError;
 });
+
+class Forecast {
+  constructor(data){
+    this.date = data.datetime;
+    this.description = `Low of ${data.low_temp}, high of ${data.high_temp} with ${data.weather.description.toLowerCase()}.`;
+  }
+}
+
+class Movie {
+  constructor(data){
+    this.title = data.title;
+    this.overview = data.overview;
+    this.vote_average = data.vote_average;
+    this.vote_count = data.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+    this.popularity = data.popularity;
+    this.release_date = data.release_date;
+  }
+}
 
 app.use((error, request, response, next) => { // eslint-disable-line
   response.status(error.status).send(`${error.status}: ${error.message}`);
