cf08bc214ae01e516c2cef13a9a307d60a87edc3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-25 15:46:31 -0800

    Bugfixed weather.js

--- modules/weather.js
+++ modules/weather.js
@@ -1,29 +1,33 @@
 'use strict';
 
+const axios = require('axios');
+
+require('dotenv').config();
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 let cache = require('./cache.js');
 
-module.exports = getWeather;
-
-function getWeather(latitude, longitude) {
-  const key = 'weather-' + latitude + longitude;
+function getWeather(lat, lon) {
+  const key = 'weather-' + lat + lon;
   const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
 
-  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+  if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
     console.log('Cache hit');
   } else {
     console.log('Cache miss');
     cache[key] = {};
     cache[key].timestamp = Date.now();
-    cache[key].data = axios.get(url)
-    .then(response => parseWeather(response.data));
+    cache[key].data = axios
+      .get(url)
+      .then((response) => parseWeather(response.data));
   }
-  
+
   return cache[key].data;
 }
 
 function parseWeather(weatherData) {
   try {
-    const weatherSummaries = weatherData.data.map(day => {
+    const weatherSummaries = weatherData.data.map((day) => {
       return new Weather(day);
     });
     return Promise.resolve(weatherSummaries);
@@ -34,7 +38,9 @@ function parseWeather(weatherData) {
 
 class Weather {
   constructor(day) {
-    this.forecast = day.weather.description;
-    this.time = day.datetime;
+    this.description = day.weather.description;
+    this.date = day.datetime;
   }
 }
+
+module.exports = getWeather;
